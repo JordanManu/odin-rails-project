93f5e0af650107700f093d086e251d3a74eff0b4
Jordan <jordanmanu@live.com.au>
2022-06-21 13:27:25 +0100

    fixed typo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenitcate_with name: "dhh", password: "secret", 
+  http_basic_authenticate_with name: "dhh", password: "secret", 
   except: [:index, :show]
-  
+
   def index
     @articles = Article.all
   end
